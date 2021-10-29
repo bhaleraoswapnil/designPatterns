b5aa6f4f1f5fdfb632898c4f3e67c5c36297e50a
swapnil <bhalerao.swapnil11@gmail.com>
2021-10-29 19:45:16 +0530

    Initial Commit

---
+++ designPattern/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  designPattern
+//
+//  Created by Swapnil Bhalerao on 29/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
